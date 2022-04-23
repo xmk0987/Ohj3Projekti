e1a3589638014ce97021c9e15f3118f2f65bfc04
ftonvi <onni.vitikainen@tuni.fi>
2022-04-23 10:59:50 +0300

    Fixed names

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Sisu.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Sisu.java
@@ -32,31 +32,35 @@ public class Sisu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+
         // Alkumenu
         Label welcome=new Label("Tervetuloa Sus Sisuun");
-        Label id=new Label("Opiskelijatunnus tai sähköposti");
-        Label password = new Label("Salasana");
+        Label name =new Label("Nimi");
+        Label student_number = new Label("Opiskelijanumero");
         welcome.setTextFill(Color.PURPLE);
         TextField namef=new TextField();
-        PasswordField passf=new PasswordField();
-        namef.setPromptText("Syötä nimi");
-        passf.setPromptText("Syötä salasana");
+        PasswordField student_numberf=new PasswordField();
+        namef.setPromptText("Syötä nimesi");
+        student_numberf.setPromptText("Syötä opiskelijanumero");
         Button okbutton = new Button("OK");
         GridPane root = new GridPane();
         root.addRow(0, welcome);
-        root.addRow(1, id, namef);
-        root.addRow(2, password, passf);
+        root.addRow(1, name, namef);
+        root.addRow(2, student_number, student_numberf);
         root.addRow(2, okbutton);
         root.setAlignment(Pos.CENTER);
 
+        // Alkuscenen aukaisu
         Scene scene=new Scene(root,400,130);
         stage.setScene(scene);
         stage.setTitle("Sus Sisu");
         stage.show();
 
+
         // Sisun pääikkuna
         GridPane maingrid = new GridPane();
         Scene mainscene=new Scene(maingrid,500,500);
+
 
         // Väärä salasana/nimi Dialog -ikkuna
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -64,11 +68,13 @@ public class Sisu extends Application {
         alert.setHeaderText("Couldn't log in!");
         alert.setContentText("Wrong email or password!");
 
+
         // Oikea salasana/nimi Dialog -ikkuna
         Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
         confirmation.setTitle("Confirmation");
         confirmation.setHeaderText("Log in Succesfull");
         okbutton.setOnAction(e -> {confirmation.showAndWait();});
+
 
         // Loading dialog
         Dialog<Pair<String, String>> dialog = new Dialog<>();
@@ -81,39 +87,33 @@ public class Sisu extends Application {
         grid.add(vBox, 1, 0);
         grid.setHgap(10);
         grid.setVgap(10);
-        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
+
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK);
         dialog.getDialogPane().setContent(grid);
+
 
         // Tarkistaa onko salasana tyhjä vai ei. Avaa dialog-ikkunan sen mukaan.
         okbutton.setOnAction(e -> {
             String name_value = namef.getText();
-            String password_value = passf.getText();
-            if(Objects.equals(name_value.trim(), "") ||Objects.equals(password_value.trim(), "")){
+            String student_number_value = student_numberf.getText();
+            if(Objects.equals(name_value.trim(), "") ||Objects.equals(student_number_value.trim(), "")){
                 alert.show();
             }
             else{
                 dialog.show();
-                PauseTransition delay = new PauseTransition(Duration.seconds(5));
+                PauseTransition delay = new PauseTransition(Duration.seconds(1));
                 delay.setOnFinished( event -> dialog.close() );
                 delay.play();
 
                 // Pääikkuna aukeaa latauksen jälkeen
-                PauseTransition delay2 = new PauseTransition(Duration.seconds(5));
+                PauseTransition delay2 = new PauseTransition(Duration.seconds(1));
                 delay2.setOnFinished( event -> stage.setScene(mainscene) );
                 delay2.play();
 
-
-
-
             }
             namef.clear();
-            passf.clear();
+            student_numberf.clear();
         });
-
-
-
-
-
 
     }
 
